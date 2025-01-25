6be1e678f3c0206dbc8de40b7b2727ade15771ca
Ashish yaduvanshi <ashish.y8750@gmail.com>
2025-01-25 21:58:42 +0530

    Initial Commit

---
+++ database/connect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://saleselastomech:tsxwt82frHNey8zg@cluster0.m3geq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
